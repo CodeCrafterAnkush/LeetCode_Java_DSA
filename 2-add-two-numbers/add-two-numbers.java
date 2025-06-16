0d68590a3a03fb31e6d28e8a5c7068cb0e1f571f
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-06-16 20:07:04 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (20.74%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -12,39 +12,38 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ansNode = new ListNode();
         ListNode head = ansNode;
-        int carry =0;
-        while(l1 != null || l2 != null){
+        int carry = 0;
+        while (l1 != null || l2 != null) {
             ListNode tempNode = new ListNode();
             int sum = 0;
-            if(l1 == null && l2 != null){
+            if (l1 == null && l2 != null) {
                 sum = l2.val + carry;
                 l2 = l2.next;
-            }else if(l1 != null && l2 == null){
-                sum = l1.val+carry;
+            } else if (l1 != null && l2 == null) {
+                sum = l1.val + carry;
                 l1 = l1.next;
-            }else{
-                sum = l1.val +l2.val + carry;
+            } else {
+                sum = l1.val + l2.val + carry;
                 l1 = l1.next;
                 l2 = l2.next;
             }
-            if(sum >= 10){
+            if (sum >= 10) {
                 carry = sum / 10;
                 int ans = sum % 10;
                 tempNode.val = ans;
                 head.next = tempNode;
-                head = head.next; 
-            }else{
+                head = head.next;
+            } else {
                 tempNode.val = sum;
                 head.next = tempNode;
                 head = head.next;
                 carry = 0;
             }
-
-            if(l1 == null && l2 == null && carry !=0){
-               ListNode carryNode = new ListNode();
-               carryNode.val = carry;
-               head.next = carryNode;
-            }
+        }
+        if (l1 == null && l2 == null && carry != 0) {
+            ListNode carryNode = new ListNode();
+            carryNode.val = carry;
+            head.next = carryNode;
         }
 
         return ansNode.next;

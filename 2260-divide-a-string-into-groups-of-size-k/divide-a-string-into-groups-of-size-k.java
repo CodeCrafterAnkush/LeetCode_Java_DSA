2afb0d0b76788d2ddaa62c8adab55a1c05f74db7
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-06-24 12:51:21 +0530

    Time: 1 ms (91.31%) | Memory: 42.4 MB (76.58%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,25 +1,41 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        List<String> stringList = new ArrayList<>();
-        int i=0;
-        StringBuilder str = new StringBuilder();
-        while(i<s.length()){
-            // str += s.charAt(i);
-            str.append(s.charAt(i));
-            if(str.length() == k){
-                stringList.add(str.toString());
-                str = new StringBuilder();
-            }
-            if(i==s.length()-1 && str.length() < k && str.length() > 0){
-                while(str.length()<k){
-                    str.append(fill);
-                }
+        // List<String> stringList = new ArrayList<>();
+        // int i=0;
+        // StringBuilder str = new StringBuilder();
+        // while(i<s.length()){
+        //     str.append(s.charAt(i));
+        //     if(str.length() == k){
+        //         stringList.add(str.toString());
+        //         str = new StringBuilder();
+        //     }
+        //     if(i==s.length()-1 && str.length() < k && str.length() > 0){
+        //         while(str.length()<k){
+        //             str.append(fill);
+        //         }
                 
-                stringList.add(str.toString());
-            }
-            i++;
-        }        
+        //         stringList.add(str.toString());
+        //     }
+        //     i++;
+        // }    
 
-         return stringList.toArray(new String[0]);
+        StringBuilder str = new StringBuilder(s);
+
+        while(str.length()%k != 0){
+            str.append(fill);
+        }
+        StringBuilder tempStr = new StringBuilder();
+        String [] stringArray = new String[str.length()/k];
+        int j=0;
+        for(int i=0;i<str.length();i++){
+            tempStr.append(str.charAt(i));
+            if(tempStr.length()==k){
+                stringArray[j] = tempStr.toString();
+                tempStr = new StringBuilder();
+                j++;
+            }
+        }
+
+         return stringArray;
     }
 }

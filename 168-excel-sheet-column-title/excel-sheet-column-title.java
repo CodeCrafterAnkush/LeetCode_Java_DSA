d3b581c9eb4ee189d64cb3e3f8e5ef66eee801a4
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-06-27 21:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (71.51%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,12 @@
+class Solution { 
+    public String convertToTitle(int columnNumber) {
+        StringBuilder s= new StringBuilder();
+       while(columnNumber>=1){
+            int r=(columnNumber-1) % 26;
+            s.append((char)(r+'A'));
+            columnNumber=(columnNumber-1)/26;
+       }
+
+       return s.reverse().toString();
+    }
+}

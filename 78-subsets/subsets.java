bc4772358c2a7664eaa015a0e65d4a0b4099f59f
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-08 21:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (82.21%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,33 @@
 class Solution {
-    public void helper( List<List<Integer>> ansList, int idx, List<Integer> temp, int nums[] ){
+    // public void helper( List<List<Integer>> ansList, int idx, List<Integer> temp, int nums[] ){
 
-        if(idx == nums.length){
-            ansList.add(new ArrayList<>(temp));
-            return;
-        }
+    //     if(idx == nums.length){
+    //         ansList.add(new ArrayList<>(temp));
+    //         return;
+    //     }
 
-        temp.add(nums[idx]);
-        helper(ansList, idx+1, temp, nums);
+    //     temp.add(nums[idx]);
+    //     helper(ansList, idx+1, temp, nums);
 
-        temp.remove(temp.size()-1);
-        helper(ansList, idx+1, temp, nums);
+    //     temp.remove(temp.size()-1);
+    //     helper(ansList, idx+1, temp, nums);
 
-    }
+    // }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ansList = new ArrayList<>();
         
-        helper(ansList , 0, new ArrayList<>(), nums);
+        // helper(ansList , 0, new ArrayList<>(), nums);
+        int n= nums.length;
+        int subset = 1 << n;
+        for(int num = 0;num<subset;num++){
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                if((num & (1<<i))!=0){
+                    list.add(nums[i]);
+                }
+            }
+            ansList.add(list);
+        }
         return ansList;
         
     }

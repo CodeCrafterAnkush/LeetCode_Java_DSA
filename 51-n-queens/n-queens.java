bc3d0b68890cccb2c29668c7b16f8ec7c7538b95
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-21 14:08:06 +0530

    Time: 2 ms (87.38%) | Memory: 44.9 MB (74.64%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean isSafe(int row,int col, char [][]board, int n){
+        int currRow = row;
+        int currCol = col;
+
+        //check upper left part
+        while(col>=0 && row >=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+            row--;
+        }
+
+        // left check
+        row = currRow;
+        col = currCol;
+        while(col>=0){
+            if(board[row][col]=='Q')return false;
+            col--;
+        }
+
+        //lower left part check
+        row = currRow;
+        col = currCol;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q')return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+    public void addFinalList(char [][]board,List<List<String>> ansList, int n){
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<n;i++ ){
+            list.add(new String(board[i]));
+        }
+        ansList.add(list);
+        return;
+
+    }
+    public void placeQueens(int col, char [][]board, List<List<String>> ansList, int n){
+        if(col == n){
+            addFinalList(board,ansList,n);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                placeQueens(col + 1, board, ansList, n);
+                board[row][col] = '.';
+            }
+        }
+
+    }
+    public List<List<String>> solveNQueens(int n) {
+        char [][] board = new char[n][n];
+        List<List<String>> ansList = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        placeQueens(0,board,ansList,n);
+       return ansList;
+    }
+}

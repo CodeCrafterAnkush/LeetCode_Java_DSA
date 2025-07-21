9076bc0a79f9bb72a1ecf3a9b44f78df655ae57d
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-21 20:10:31 +0530

    Time: 1 ms (99.63%) | Memory: 44.8 MB (74.64%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,49 +1,41 @@
 class Solution {
-    public boolean isSafe(int row, int col, char[][] board, int n) {
-        int currRow = row;
-        int currCol = col;
+    // Approach-1
 
-        //check upper left part
-        while (col >= 0 && row >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-            row--;
-        }
+    // public boolean isSafe(int row, int col, char[][] board, int n) {
+    //     int currRow = row;
+    //     int currCol = col;
 
-        // left check
-        row = currRow;
-        col = currCol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-        }
-
-        //lower left part check
-        row = currRow;
-        col = currCol;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row++;
-            col--;
-        }
-
-        return true;
-    }
-
-    // public void addFinalList(char[][] board, List<List<String>> ansList, int n) {
-    //     List<String> list = new ArrayList<>();
-    //     for (int i = 0; i < n; i++) {
-    //         list.add(new String(board[i]));
+    //     //check upper left part
+    //     while (col >= 0 && row >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         col--;
+    //         row--;
     //     }
-    //     ansList.add(list);
-    //     return;
 
+    //     // left check
+    //     row = currRow;
+    //     col = currCol;
+    //     while (col >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         col--;
+    //     }
+
+    //     //lower left part check
+    //     row = currRow;
+    //     col = currCol;
+    //     while (row < n && col >= 0) {
+    //         if (board[row][col] == 'Q')
+    //             return false;
+    //         row++;
+    //         col--;
+    //     }
+
+    //     return true;
     // }
 
-    public void placeQueens(int col, char[][] board, List<List<String>> ansList, int n) {
+    public void placeQueens(int col, char[][] board, List<List<String>> ansList, int n, int [] leftCheck, int [] upperLeftCheck,int [] lowerLeftCheck) {
         if (col == n) {
             List<String> list = new ArrayList<>();
             for (int i = 0; i < n; i++) {
@@ -54,10 +46,27 @@ class Solution {
         }
 
         for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, board, n)) {
+
+            //Approach-1
+
+            // if(isSafe(row, col, board, n)) {
+            //     board[row][col] = 'Q';
+            //     placeQueens(col + 1, board, ansList,n);
+            //     board[row][col] = '.';
+            // }
+
+            
+            //Approach-2
+            if (leftCheck[row]==0 && upperLeftCheck[row+col]==0 && lowerLeftCheck[(n-1)+(col-row)]==0) {
                 board[row][col] = 'Q';
-                placeQueens(col + 1, board, ansList, n);
+                leftCheck[row]=1;
+                upperLeftCheck[row+col]=1;
+                lowerLeftCheck[(n-1)+(col-row)]=1;
+                placeQueens(col + 1, board, ansList,n,leftCheck,upperLeftCheck,lowerLeftCheck);
                 board[row][col] = '.';
+                leftCheck[row]=0;
+                upperLeftCheck[row+col]=0;
+                lowerLeftCheck[(n-1)+(col-row)]=0;
             }
         }
 
@@ -65,11 +74,14 @@ class Solution {
 
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
+        int [] leftCheck = new int[n];
+        int [] upperLeftCheck = new int[(2*n)-1];
+        int [] lowerLeftCheck = new int[(2*n)-1];
         List<List<String>> ansList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
-        placeQueens(0, board, ansList, n);
+        placeQueens(0, board, ansList, n,leftCheck,upperLeftCheck,lowerLeftCheck);
         return ansList;
     }
 }

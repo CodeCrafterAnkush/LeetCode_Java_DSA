9ea126cef4882ddc56fa3ad07d3da635def51803
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-26 19:37:59 +0530

    Time: 17 ms (86.97%) | Memory: 94.6 MB (75.95%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,29 +1,18 @@
 class Solution {
-    public void combo(int num,int k,int n,List<Integer> li,Set<List<Integer>> set){
+    public void combo(int num,int k,int n,List<Integer> li,List<List<Integer>> result){
         if(k==0){
-            Collections.sort(li);
-            set.add(new ArrayList<>(li));
+            result.add(new ArrayList<>(li));
             return;
         }
         for(int i=num;i<=n;i++){
             li.add(i);
-            combo(i+1,k-1,n,li,set);
+            combo(i+1,k-1,n,li,result);
             li.remove(li.size()-1);
         }
     }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i=1;i<=n;i++){
-            List<Integer> li = new ArrayList<>();
-            li.add(i);
-            combo(i+1,k-1,n,li,set);
-            li.remove(li.size()-1);
-        }
-
-        for(List<Integer> li : set){
-            result.add(new ArrayList<>(li));
-        }
+            combo(1,k,n,new ArrayList<>(),result);
         return result;
     }
 }

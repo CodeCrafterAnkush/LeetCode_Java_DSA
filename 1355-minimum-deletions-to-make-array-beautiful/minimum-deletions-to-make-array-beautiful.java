59deaf9f2dfe5960a5faf8faa3ccba2458d464d2
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-27 21:07:13 +0530

    Time: 4 ms (55.14%) | Memory: 57.6 MB (68.11%) - LeetSync

---
+++ 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDeletion(int[] nums) {
+      int minDeletions =0;
+      for(int i=0;i<nums.length-1;i++){
+        if((i-minDeletions) % 2 == 0){
+            if(nums[i]==nums[i+1]){
+                minDeletions++;
+            }
+        }
+      }
+      if((nums.length-minDeletions) % 2 != 0){
+        minDeletions++;
+      }
+      return minDeletions;  
+    }
+}

3e18b6c3ea282e8633d8c8fbcda4c4f09ce06191
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-30 21:13:26 +0530

    Time: 3 ms (82.57%) | Memory: 60.9 MB (48.13%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,6 +1,5 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        // Arrays.sort(nums);
         if(nums.length==1)return 1;
         int maxIdx = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -14,66 +13,10 @@ class Solution {
             if(nums[i] == maxVal){
                 subArrCount++;
                 maxLength = Math.max(subArrCount,maxLength);
-                // if(i!=nums.length-1){
-                //     if(nums[i]==nums[i+1]){
-                //         subArrCount++;
-                //         maxLength = Math.max(subArrCount,maxLength);
-                //     }else{
-                //         suArrCount
-                //     }
-                // }
             }else{
                 subArrCount =0;
             }
         }
-        // for(int i : nums)System.out.println(i);
-        
-        // System.out.println(nums[maxIdx]);
-        // System.out.println(maxIdx);
-
-        int length = 1;
-        // if (maxIdx == nums.length - 1) {
-        //     int i = nums.length - 2;
-        //     int maxEle = nums[maxIdx];
-        //     while (maxEle == nums[i] && i >= 0) {
-        //         length++;
-        //         if(i==0)break;
-        //         i--;
-        //     }
-        // }
-
-        // if (maxIdx == 0) {
-        //     int i = 1;
-        //     int maxEle = nums[0];
-        //     while (maxEle == nums[i] && i < nums.length) {
-        //         length++;
-        //         if(i==nums.length-1)break;
-        //         i++;
-        //     }
-        // } else if (maxIdx == nums.length - 1) {
-        //     int i = nums.length - 2;
-        //     int maxEle = nums[nums.length - 1];
-        //     while (maxEle == nums[i] && i >= 0) {
-        //         length++;
-        //         if(i==0)break;
-        //         i--;
-        //     }
-        // } else if (nums[maxIdx] == nums[maxIdx + 1]) {
-        //     int i = maxIdx + 1;
-        //     int maxEle = nums[maxIdx];
-        //     while (maxEle == nums[i] && i < nums.length) {
-        //         length++;
-        //         if(i==nums.length-1)break;
-        //         i++;
-        //     }
-        // }else if(nums[maxIdx]==nums[maxIdx-1]){
-        //     int i = maxIdx-1;
-        //     int maxEle = nums[maxIdx];
-        //     while (maxEle == nums[i] && i >= 0) {
-        //         length++;
-        //         i--;
-        //     }
-        // }
         return maxLength;
     }
 }

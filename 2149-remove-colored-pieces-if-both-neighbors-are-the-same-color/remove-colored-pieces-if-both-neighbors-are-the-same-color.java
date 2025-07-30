50c31503cf43fb3687c7d6918bdd025b1e0a4f28
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-07-30 14:25:10 +0530

    Time: 9 ms (86.74%) | Memory: 44.9 MB (98.94%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
+        char arr[] = colors.toCharArray();
         int turnA=0;
         int turnB=0;
         for(int i=1;i<colors.length()-1;i++){
-            if(colors.charAt(i)=='A' && colors.charAt(i)==colors.charAt(i-1)&& colors.charAt(i)==colors.charAt(i+1))turnA++;
-            if(colors.charAt(i)=='B' && colors.charAt(i)==colors.charAt(i-1)&& colors.charAt(i)==colors.charAt(i+1))turnB++;
+            if(arr[i]=='A' && arr[i]==arr[i-1] && arr[i]==arr[i+1])turnA++;
+            if(arr[i]=='B' && arr[i]==arr[i-1] && arr[i]==arr[i+1])turnB++;
         }
 
         return (turnA>turnB)?true:false;

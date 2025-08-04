ace2cd1c477c896c111998bde90725cd1371d767
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-04 12:47:23 +0530

    Time: 6 ms (72.37%) | Memory: 44.7 MB (23.65%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,17 +1,7 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        Set<Integer> set = new HashSet<>();
         Map<Integer,Integer> map = new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     set.add(nums[i]);
-        // }
-        // ArrayList<Integer> list = new ArrayList<>();
-        // for(int n : set){
-        //     list.add(n);
-        // }
-        // Collections.sort(list);
         int arr[]= new int [nums.length];
-
         for(int i=0;i<arr.length;i++){
            arr[i]=nums[i];
         }
@@ -23,20 +13,11 @@ class Solution {
             map.put(arr[i],map.get(arr[i]));
            }
         }
-
         for(int i=0;i<nums.length;i++){
            if(map.containsKey(nums[i])){
             arr[i]=map.get(nums[i]);
            }
         }
-
-    //   for(int i=0;i<nums.length;i++){
-    //     int count=0;
-    //     for(int j=0;j<nums.length;j++){
-    //         if(nums[i]>nums[j])count++;
-    //     }
-    //     arr[i]=count;
-    //   }  
       return arr;
     }
 }

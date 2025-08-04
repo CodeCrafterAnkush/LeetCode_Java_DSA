1045fddc974d905ec2dce91e20122041e92883ad
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-04 12:43:42 +0530

    Time: 7 ms (70.62%) | Memory: 44.9 MB (13.19%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,13 +1,42 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int arr[]=new int[nums.length];
-      for(int i=0;i<nums.length;i++){
-        int count=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[i]>nums[j])count++;
+        Set<Integer> set = new HashSet<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     set.add(nums[i]);
+        // }
+        // ArrayList<Integer> list = new ArrayList<>();
+        // for(int n : set){
+        //     list.add(n);
+        // }
+        // Collections.sort(list);
+        int arr[]= new int [nums.length];
+
+        for(int i=0;i<arr.length;i++){
+           arr[i]=nums[i];
         }
-        arr[i]=count;
-      }  
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+           if(!map.containsKey(arr[i])){
+            map.put(arr[i],i);
+           }else{
+            map.put(arr[i],map.get(arr[i]));
+           }
+        }
+
+        for(int i=0;i<nums.length;i++){
+           if(map.containsKey(nums[i])){
+            arr[i]=map.get(nums[i]);
+           }
+        }
+
+    //   for(int i=0;i<nums.length;i++){
+    //     int count=0;
+    //     for(int j=0;j<nums.length;j++){
+    //         if(nums[i]>nums[j])count++;
+    //     }
+    //     arr[i]=count;
+    //   }  
       return arr;
     }
 }

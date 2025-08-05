2d68bba176b7c906b984b0ceac42a156091f6913
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-05 20:50:39 +0530

    Time: 1 ms (46.13%) | Memory: 41.1 MB (6.15%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countStep(long n,HashMap<Long,Integer>map){
+        if(n==1)return 0;
+        if(map.containsKey(n)) return map.get(n);
+        int steps;
+        if(n%2 ==0){
+            steps = 1+ countStep(n/2,map);
+        }else{
+            steps = 1+Math.min(countStep(n+1,map),countStep(n-1,map));
+        } 
+        map.put(n,steps);
+        return steps;       
+    }
+    public int integerReplacement(int n) {
+        HashMap<Long,Integer> map = new HashMap<>();
+        return countStep((long)n,map);
+    }
+}

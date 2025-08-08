50cbdc55511eb04c6f214e6ccb6f08adc87c95a1
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-08 20:30:49 +0530

    Time: 5 ms (89.58%) | Memory: 44.4 MB (68.04%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,29 +1,46 @@
 class Solution {
-    public HashSet<Integer> uniqueNumbers = new HashSet<>();
-    public void count3DigitUniqueEvenNo(int[]digits, StringBuilder sb, boolean[]visited){
-        if(sb.length()==3){
-            int n = Integer.parseInt(sb.toString());
-            if(n % 2 ==0)uniqueNumbers.add(n);
-            return;
-        }
+    // public HashSet<Integer> uniqueNumbers = new HashSet<>();
+    // public void count3DigitUniqueEvenNo(int[]digits, StringBuilder sb, boolean[]visited){
+    //     if(sb.length()==3){
+    //         int n = Integer.parseInt(sb.toString());
+    //         if(n % 2 ==0)uniqueNumbers.add(n);
+    //         return;
+    //     }
 
-        for(int i=0;i<digits.length;i++){
-            if(sb.length()==0 && digits[i]==0)continue;
-            if(!visited[i]){
-                visited[i]=true;
-                sb.append(digits[i]);
-                count3DigitUniqueEvenNo(digits,sb,visited);
-                sb.deleteCharAt(sb.length() - 1);
-                visited[i]=false;
+    //     for(int i=0;i<digits.length;i++){
+    //         if(sb.length()==0 && digits[i]==0)continue;
+    //         if(!visited[i]){
+    //             visited[i]=true;
+    //             sb.append(digits[i]);
+    //             count3DigitUniqueEvenNo(digits,sb,visited);
+    //             sb.deleteCharAt(sb.length() - 1);
+    //             visited[i]=false;
 
+    //         }
+    //     }
+
+    // }
+    public int totalNumbers(int[] digits) {
+        // boolean [] visited = new boolean[digits.length];
+        // StringBuilder sb = new StringBuilder();
+        // count3DigitUniqueEvenNo(digits,sb,visited);
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        int n=digits.length;
+        for(int i=0;i<n;i++){
+            if(digits[i]==0)continue;
+            for(int j=0;j<n;j++){
+                if(j==i)continue;
+                for(int k=0;k<n;k++){
+                    if(k==i || k==j)continue;
+                    if(digits[k] % 2 == 0){
+                        int digit = digits[i]*100+digits[j]*10+digits[k];
+                    uniqueNumbers.add(digit);
+                    }
+                    
+                }
             }
         }
-
-    }
-    public int totalNumbers(int[] digits) {
-        boolean [] visited = new boolean[digits.length];
-        StringBuilder sb = new StringBuilder();
-        count3DigitUniqueEvenNo(digits,sb,visited);
         return uniqueNumbers.size();
+
     }
 }

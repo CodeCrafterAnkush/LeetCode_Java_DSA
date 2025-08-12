0cb746008b674a8dc41205d8e8395dc7f2992d09
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-12 21:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.10%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,17 +1,35 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int i=0,index=0;
-        while(i<=nums.length-1){
-            if(nums[i]==target){
-                return i;
-            }else if(nums[nums.length-1]<target){
-                     return nums.length;
-                }else if(nums[i]<target) {
-                    index++ ;
-                }
-            
-            i++;
+    public int lowerBound(int arr[],int target,int n){
+        int low =0;
+        int high = n-1;
+        int ans = n;
+        while(low<= high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
         }
-        return index;
+        return ans;
+    }
+    public int searchInsert(int[] nums, int target) {
+        // int i=0,index=0;
+        // while(i<=nums.length-1){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }else if(nums[nums.length-1]<target){
+        //              return nums.length;
+        //         }else if(nums[i]<target) {
+        //             index++ ;
+        //         }
+            
+        //     i++;
+        // }
+        // return index;
+
+        //-------OR___________
+        return lowerBound(nums,target,nums.length);
     }
 }

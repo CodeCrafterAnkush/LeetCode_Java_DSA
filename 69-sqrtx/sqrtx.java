08d772b04738e33a337e926104f165f3e5690ef9
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-17 19:41:17 +0530

    Time: 1 ms (84.37%) | Memory: 40.6 MB (97.68%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -5,15 +5,33 @@ class Solution {
         
 
         // TIME COMPLEXITY is -- 0(√n)
+        // if(n ==0)return 0;
+        // int ans = 1;
+        // for(long i=1;i<=n;i++){
+        //     if(i*i<=n){
+        //         ans = (int)i;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return ans;
+        // _______OR_________
+
+        //  TIME COMPLEXITY is -- 0(log n)
         if(n ==0)return 0;
+        long low =1;
+        long high =(long)n;
         int ans = 1;
-        for(long i=1;i<=n;i++){
-            if(i*i<=n){
-                ans = (int)i;
+        while(low<=high){
+            long mid = (low+high)/2;
+            if(mid * mid <= n){
+                ans = (int)mid;
+                low = mid+1;
             }else{
-                break;
+                high = mid-1;
             }
         }
+
         return ans;
     }
 }

145d5567be03086a8803a8b2131c5203b6218f11
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-17 19:17:49 +0530

    Time: 21 ms (7.51%) | Memory: 40.9 MB (69.17%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
-    public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+    public int mySqrt(int n) {
+        // return (int)Math.sqrt(x); 
+        // -------OR-------
+        
+
+        // TIME COMPLEXITY is -- 0(√n)
+        if(n ==0)return 0;
+        int ans = 1;
+        for(long i=1;i<=n;i++){
+            if(i*i<=n){
+                ans = (int)i;
+            }else{
+                break;
+            }
+        }
+        return ans;
     }
 }

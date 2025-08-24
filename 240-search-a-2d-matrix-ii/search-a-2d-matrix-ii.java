057e52905103339e47fc0f9f75bd940312787b4b
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-24 19:10:26 +0530

    Time: 6 ms (48.88%) | Memory: 46.2 MB (39.05%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -11,11 +11,23 @@ class Solution {
         return false;
     }
     public boolean searchMatrix(int[][] mat, int target) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        for(int i=0;i<rows;i++){
-            if(binarySearch(mat[i],target))return true;
+        // Approach - 1
+        // int rows = mat.length;
+        // int cols = mat[0].length;
+        // for(int i=0;i<rows;i++){
+        //     if(binarySearch(mat[i],target))return true;
+        // }
+
+        // Approach - 2
+        int n = mat.length;
+        int row = 0;
+        int col = mat[0].length-1;
+        while(row < n && col >= 0){
+            if(mat[row][col] == target)return true;
+            else if(mat[row][col]> target)col--;
+            else row++;
         }
+
         return false;
     }
 }

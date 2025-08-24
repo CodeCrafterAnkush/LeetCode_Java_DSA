92fe36c0eb7ff3161bcff3a8efbc94ab11cffa8b
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-08-24 19:12:11 +0530

    Time: 6 ms (48.88%) | Memory: 46.3 MB (21.58%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,15 +1,5 @@
 class Solution {
-    // public boolean binarySearch(int arr[],int target){
-    //     int low=0;
-    //     int high = arr.length-1;
-    //     while(low<=high){
-    //         int mid =(low+high)/2;
-    //         if(arr[mid]==target)return true;
-    //         else if(arr[mid]<target)low = mid+1;
-    //         else high = mid-1;
-    //     }
-    //     return false;
-    // }
+    
     public boolean searchMatrix(int[][] mat, int target) {
         // Approach - 1
         // int rows = mat.length;

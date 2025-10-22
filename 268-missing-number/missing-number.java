42ed9600fdba63969a3210804c69abdb26e9700d
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-10-22 17:29:27 +0530

    Time: 1 ms (31.90%) | Memory: 45.3 MB (58.29%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -10,12 +10,21 @@ class Solution {
         // return sum1-sum2;
 
         // method-2
-        int xor1 = 0;
-        for(int i=1;i<=n;i++){
-            xor1 ^= i;
-        }
-        int xor2=0;
+        // int xor1 = 0;
+        // for(int i=1;i<=n;i++){
+        //     xor1 ^= i;
+        // }
+        // int xor2=0;
+        // for(int i=0;i<n;i++){
+        //     xor2 ^= nums[i];
+        // }
+        // return xor1 ^ xor2;
+
+        // method-3 
+        int xor1=0;
+        int xor2 =0;
         for(int i=0;i<n;i++){
+            xor1 ^= (i+1);
             xor2 ^= nums[i];
         }
         return xor1 ^ xor2;

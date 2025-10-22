5bdd9bb23ab558527f1556f74ee315e396a777f8
Ankush Satpute <ankushsatpute2005@gmail.com>
2025-10-22 17:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (86.19%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,23 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        //method-1
         int n = nums.length;
-        int sum2=0;
-        int sum1 = (n*(n+1))/2;
-        for(int i=0;i<n;i++){
-            sum2+=nums[i];
+        // int sum2=0;
+        // int sum1 = (n*(n+1))/2;
+        // for(int i=0;i<n;i++){
+        //     sum2+=nums[i];
+        // }
+        // return sum1-sum2;
+
+        // method-2
+        int xor1 = 0;
+        for(int i=1;i<=n;i++){
+            xor1 ^= i;
         }
-        return sum1-sum2;
+        int xor2=0;
+        for(int i=0;i<n;i++){
+            xor2 ^= nums[i];
+        }
+        return xor1 ^ xor2;
     }
 }

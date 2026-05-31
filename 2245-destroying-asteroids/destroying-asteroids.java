60ec15714deaea8508e91de4fea33ad9757786f7
Ankush Satpute <ankushsatpute2005@gmail.com>
2026-05-31 18:30:24 +0530

    Time: 25 ms (93.30%) | Memory: 107.7 MB (7.85%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -2,11 +2,18 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] astros) {
         Arrays.sort(astros);
         long m = mass;
-        int i=0;
-        while(i<astros.length){
-            if(m >= astros[i]) m+= astros[i];
-            else return false;
-            i++;
+        // int i=0;
+        // while(i<astros.length){
+        //     if(m >= astros[i]) m+= astros[i];
+        //     else return false;
+        //     i++;
+        // }
+
+        for(int astroid :astros){
+            if(m-astroid<0){
+                return false;
+            }
+            m+=astroid;
         }
 
         return true;

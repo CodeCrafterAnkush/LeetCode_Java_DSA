52511f7e6f339a987db493b638a3ea73bf780b8c
Ankush Satpute <ankushsatpute2005@gmail.com>
2026-06-22 12:13:12 +0530

    Time: 2 ms (96.67%) | Memory: 43.2 MB (79.23%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,15 +1,22 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character, Integer> map = new HashMap<>();
+        int countB =0 ;
+        int countA =0;
+        int countL = 0;
+        int countO = 0;
+        int countN = 0;
+
         for(char ch:text.toCharArray()){
-            if(ch == 'b'|| ch == 'a' || ch == 'l' || ch == 'o' || ch == 'n'){
-                map.put(ch,map.getOrDefault(ch,0)+1);
-            }
+            if(ch == 'b') countB++;
+            else if(ch=='a') countA++;
+            else if(ch =='l') countL++;
+            else if(ch == 'o') countO++;
+            else if(ch == 'n')countN++;
         }
         // b,a,n
-        int singleCh = Math.min(map.getOrDefault('b',0),Math.min(map.getOrDefault('a',0),map.getOrDefault('n',0)));
+        int singleCh = Math.min(countB,Math.min(countA,countN));
         // l,o
-        int doubleCh = Math.min(map.getOrDefault('l',0),map.getOrDefault('o',0));
+        int doubleCh = Math.min(countO,countL);
 
         return Math.min(doubleCh/2,singleCh); 
     }

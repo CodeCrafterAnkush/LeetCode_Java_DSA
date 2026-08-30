c7aadec631cc00a091fa0ab24834fdcb9e878fbc
Ankush Satpute <ankushsatpute2005@gmail.com>
2026-08-30 17:55:52 +0530

    Time: 14 ms (66.74%) | Memory: 48.8 MB (22.56%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -5,14 +5,7 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         for(int i=2*nums.length-1;i>=0;i--){
             while(!stack.isEmpty() && nums[i%n]>=nums[stack.peek()])stack.pop();
-
-            if(stack.isEmpty()){
-                arr[i%n]=-1;
-            }else{
-                arr[i%n]=nums[stack.peek()];
-            }
-
-            // arr[i%n]= stack.isEmpty()?-1:nums[stack.peek()];
+            arr[i%n]= stack.isEmpty()?-1:nums[stack.peek()];
             stack.push(i%n);
         }
         return arr;

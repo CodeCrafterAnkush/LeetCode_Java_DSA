c925ea698db94f1480c590ec3240864a962f0810
Ankush Satpute <ankushsatpute2005@gmail.com>
2026-09-03 11:29:13 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (73.96%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        // even - even = even
+        // odd - odd = even
+        // odd - even =odd
+        // even - odd = odd
         return true;
     }
 }
